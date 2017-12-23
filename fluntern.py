06fc80a790faee41147fc2b18ababf618e40b605
Frank Potthast Rutz <frank.potthast@gmail.com>
2017-12-23 08:01:53 +0000

    Update fluntern.py

--- fluntern.py
+++ fluntern.py
@@ -41,7 +41,18 @@ def makeUebergang():
         mc.setBlocks(22+x*7,0,-12 ,22+x*7,2,-12 ,1 ) 
     
 def makeSchulhaus():
-    mc.setBlocks(51,0,0  ,69,9,-60 ,1)
+    mc.setBlocks(51,0,0   ,69,12,-60 ,1)#Gebäudekörper Schulhaus, massiv, stone
+    mc.setBlocks(59,1,0   ,61,2,2    ,0)#Haupteingang
+    mc.setBlocks(52,1,-1  ,68,3,-59  ,0)#Erdgeschoss mit Luft füllen
+    mc.setBlocks(52,5,-1  ,68,7,-59  ,0)#1.OG mit Luft füllen
+    mc.setBlocks(52,9,-1  ,68,11,-59 ,0)#2.OG mit Luft füllen
+
+
+    mc.setBlocks(70,0,-5  ,77,12,-15 ,1)#Treppenhaus massiv, stone
+    mc.setBlocks(69,1,-6  ,76,11,-14 ,0)#Treppenhaus mit Luft füllen
+    mc.setBlocks(70,1,-6  ,77,1,-8   ,1)#Treppe EG zum 1.OG
+    mc.setBlocks(76,2,-9  ,74,2,-14  ,1)
+    mc.setBlocks(69,3,-14 ,73,3,-11  ,1)
 
 
 def makeFussballplatz():
